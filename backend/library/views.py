4c45bf15f046cccc2be9dfba12ac80e8f967bfd9
ElfinAndSnow <andyfucookies@gmail.com>
2023-10-11 14:50:41 +0800

    add permission setting to library.BookViewSet

--- backend/library/views.py
+++ backend/library/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from .models import Book
 from rest_framework.viewsets import ModelViewSet
 from .serializer import BookSerializer
+from rest_framework.permissions import AllowAny
 
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = []
+    permission_classes = [AllowAny]
