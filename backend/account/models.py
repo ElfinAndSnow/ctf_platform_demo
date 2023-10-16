a0a71805d9bdb28d7c6394c98165eb7820dbb690
[OrlandoFurioso] <[ark2175980882@163.com]>
2023-10-16 22:17:12 +0800

    Fix the hierachy of the files

--- backend/account/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    # objects = models.Manager()
-
-    class Meta:
-        verbose_name = "用户"
