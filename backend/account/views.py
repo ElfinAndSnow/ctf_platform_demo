0953dde9428c55dfe4a96480531e0f5cd56f32f7
ElfinAndSnow <andyfucookies@gmail.com>
2023-10-18 12:01:49 +0800

    add several verifications before creating a UserChallengeSession in UserChallengeSessionCreateView. User can only create a session of a challenge they haven't solved. User can't create a duplicate session of the same challenge if the last session of this challenge is NOT solved or expired. Optimize some response data.

--- backend/account/views.py
+++ backend/account/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from account.models import User, UserChallengeSession
 from account.serializer import UserIDSerializer, UserChallengeSessionCreateRetrieveSerializer, FlagSubmissionSerializer
+from challenge.models import Challenge
 from utils.custom_permissions import IsAdminOrSessionCreator
 
 
@@ -24,8 +25,36 @@ class UserChallengeSessionCreateView(generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         print("request data: " + str(request.data))
+        # 只能给自己创建session
         if request.user.pk != request.data['user']:
-            return Response(data="You can only create session for yourself!", status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                data={"msg": "You can only create session for yourself!"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # 如果该题目已被该用户完成，则无法创建session
+        user = request.user
+        challenge = Challenge.objects.get(id=request.data['challenge'])
+        # print(type(challenge.solved_by))
+        if user in challenge.solved_by.all():
+            return Response(
+                data={"msg": "You have already solved this challenge!"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # 如果用户已创建某一题目的session且is_solved_or_expired=False，则禁止对该题创建新session
+        user_challenge_sessions_created = UserChallengeSession.objects.filter(user=user, challenge=challenge)
+        print("user_challenge_sessions_created:" + str(user_challenge_sessions_created))
+        # filter返回一个存有若干个UserChallengeSession的QuerySet
+        # user_challenge_sessions_created:
+        # <QuerySet [<UserChallengeSession: [OPEN]user1_lunatic web_2023-10-18 02:00:30.762051+00:00>]>
+        for session_obj in user_challenge_sessions_created:
+            if not session_obj.is_solved_or_expired:
+                return Response(
+                    data={"msg": "You have created a session for this challenge!"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
@@ -55,6 +84,7 @@ class FlagSubmissionView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
+        # TODO
+        # serializer里少东西生成headers时会报错，待解决
         # headers = self.get_success_headers(serializer.data)
         return Response({"msg": "You get the correct answer!"}, status=status.HTTP_201_CREATED)
-
