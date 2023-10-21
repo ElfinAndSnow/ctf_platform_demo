a0e1ea46d9e9bab3f11381ef3edef8f53f149a14
ElfinAndSnow <andyfucookies@gmail.com>
2023-10-21 17:14:27 +0800

    update user model fields

--- backend/account/models.py
+++ backend/account/models.py
@@ -7,8 +7,6 @@ from django.db import models
 class User(AbstractUser):
     # solved_challenges = models.ManyToManyField('challenge.Challenge', related_name="solved_by", blank=True)
     points = models.IntegerField(verbose_name="个人总分数", default=0, null=True)
-    team = models.ForeignKey('team.Team', on_delete=models.SET_NULL, related_name="members", null=True, blank=True)
-    # objects = models.Manager()
     team = models.ForeignKey('team.Team', verbose_name="所属战队", on_delete=models.SET_NULL, related_name="members", null=True, blank=True)
     description = models.TextField(verbose_name="个人简介", null=True, blank=True)
     is_private = models.BooleanField(verbose_name="个人信息状态", null=True, default=False)
