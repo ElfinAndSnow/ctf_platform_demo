d7fb0b55245311c658d6871c8f96ba6abb72fe73
ElfinAndSnow <andyfucookies@gmail.com>
2023-10-21 17:16:03 +0800

    add user info update

--- backend/account/serializer.py
+++ backend/account/serializer.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 from .models import User, UserChallengeSession
 
 
-# 暂时不需要，因为login和register的视图和序列化器要单独写
-class UserSerializer(serializers.ModelSerializer):
 class UserInfoSerializer(serializers.ModelSerializer):
     # TODO TeamInfoSerializer嵌套
     # team = TeamInfoSerializer
@@ -38,6 +36,7 @@ class UserInfoSerializer(serializers.ModelSerializer):
         ]
 
 
+class UsernameUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
