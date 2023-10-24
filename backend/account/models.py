8fbf043dfb5c9cc4253637a2fd1213209fc97534
ElfinAndSnow <andyfucookies@gmail.com>
2023-10-24 14:43:59 +0800

    split is_solved_or_expired field to two fields in UserChallengeSession.

--- backend/account/models.py
+++ backend/account/models.py
@@ -49,41 +49,21 @@ class UserChallengeSession(AbstractTimeLimitedModel):
 
     user = models.ForeignKey(User, verbose_name="用户", on_delete=models.CASCADE)
     challenge = models.ForeignKey('challenge.Challenge', verbose_name="题目", on_delete=models.CASCADE)
-    start_time = models.DateTimeField(verbose_name="开始时间", auto_now_add=True)
-    time_limit = models.IntegerField(verbose_name="时间限制", default=3000)
     # 不保存用户提交的flag
     # user_flag = models.CharField(verbose_name="用户提交的flag", max_length=127, blank=True)
     # 被动检测超时或已解决，每次收到请求之前先进行一次超时检查
-    is_solved_or_expired = models.BooleanField(verbose_name="是否已解决或已过期", default=False)
-
-    def get_start_time(self):
-        return self.start_time
-
-    def get_time_limit(self):
-        return self.time_limit
+    is_solved = models.BooleanField(verbose_name="是否已解决或已过期", default=False)
 
     def get_flag(self):
         return self.challenge.flag
 
     def get_current_state(self):
-        return self.is_solved_or_expired
-
-    def expiration_verification(self):
-        start_time = self.get_start_time()
-        time_limit = self.get_time_limit()
-
-        current_time = datetime.datetime.now(datetime.timezone.utc)
-        time_elapsed = (current_time - start_time).total_seconds()
-
-        if time_elapsed > time_limit:
-            return True
-        else:
-            return False
+        return self.is_solved and self.is_expired
 
     def flag_verification(self, flag):
         correct_flag = self.get_flag()
         if flag == correct_flag:
-            self.is_solved_or_expired = True
+            self.is_solved = True
             self.save()
         return flag == correct_flag
 
@@ -96,5 +76,5 @@ class UserChallengeSession(AbstractTimeLimitedModel):
             _str += "[OPEN]"
         _str += self.user.username + "_"
         _str += self.challenge.name + "_"
-        _str += str(self.start_time)
+        _str += str(self.created_at)
         return _str
