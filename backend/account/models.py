5094ef19972553de4de953b77fe8978001b7dd35
ElfinAndSnow <andyfucookies@gmail.com>
2023-10-24 14:41:58 +0800

    added is_email_verified boolean field to User model

--- backend/account/models.py
+++ backend/account/models.py
@@ -11,7 +11,9 @@ class User(AbstractUser):
     points = models.IntegerField(verbose_name="个人总分数", default=0, null=True)
     team = models.ForeignKey('team.Team', verbose_name="所属战队", on_delete=models.SET_NULL, related_name="members", null=True, blank=True)
     description = models.TextField(verbose_name="个人简介", null=True, blank=True)
-    is_private = models.BooleanField(verbose_name="个人信息状态", null=True, default=False)
+    is_private = models.BooleanField(verbose_name="个人信息隐私状态", null=True, default=False)
+    is_email_verified = models.BooleanField(verbose_name="是否已邮箱验证", null=True, default=False)
+    # enable_password_reset = models.BooleanField(verbose_name="可否更改密码", null=True, default=False)
 
     # 隐藏的字段
     # leading_team
