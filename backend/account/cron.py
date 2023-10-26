77a964320d147e82ebc1b8ea6c56977f9691196d
ElfinAndSnow <andyfucookies@gmail.com>
2023-10-26 21:39:58 +0800

    fix UserChallengeSessionCronJob

--- backend/account/cron.py
+++ backend/account/cron.py
@@ -4,12 +4,12 @@ from account.models import UserChallengeSession
 
 
 class UserChallengeSessionCronJob(CronJobBase):
-    RUN_EVERY_MINS = 2  # every 2 minutes
-
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = 'user_challenge_session_cron_job'    # a unique code
+    schedule = Schedule(run_every_mins=1)
+    code = 'account.user_challenge_session_cron_job'    # a unique code
 
     def do(self):
+        print("running cron job.")
         sessions = UserChallengeSession.objects.filter(is_expired=False)
         for session in sessions:
+            print("doing cron job for session:" + str(session))
             session.expiration_verification()
