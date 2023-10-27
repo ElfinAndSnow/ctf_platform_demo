f62439ca0cc3f42ba03eb29111d0abfed606ced8
[OrlandoFurioso] <[ark2175980882@163.com]>
2023-10-27 15:45:31 +0800

    Add the team parts

--- backend/leaderboard/models.py
+++ backend/leaderboard/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from account.models import User
+from team.models import Team
 
 # Create your models here.
--- backend/team/urls.py
+++ backend/team/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('create_team/<str:team_name>/', views.create_team, name='create_team'),
-    # path(r'DeleteTeamView', views.DeleteTeamView.as_view(), name='DeleteTeamView'),
     path(r'DeleteTeamView', views.DeleteTeamView.as_view(), name='DeleteTeamView'),
     path('join_team/<int:team_id>/<str:invitation_token>/', views.join_team, name='join_team'),
     path('RemoveMemberView/<int:user_id>', views.RemoveMemberView.as_view(), name='RemoveMemberView'),
