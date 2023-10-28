11232f1aac8d0375cc1efd6e69b5c4d45d6f5a78
Fanglinrui <Fanglinrui@bupt.edu.cn>
2023-10-28 20:21:34 +0800

    delete unnecessary settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,10 +169,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = "/static/"
- 
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
