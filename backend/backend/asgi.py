65b4cf7e424e79d30783250982b5f62211c9e207
ElfinAndSnow <137981133+ElfinAndSnow@users.noreply.github.com>
2023-10-31 17:49:29 +0800

    Update asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
+from dotenv import load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 from django.core.asgi import get_asgi_application
 
