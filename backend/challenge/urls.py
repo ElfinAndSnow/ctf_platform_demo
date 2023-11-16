da42e3257e3788f5a2d1b8c4ec3fe4ef89d641ff
ElfinAndSnow <andyfucookies@gmail.com>
2023-11-16 12:21:58 +0800

    add file download view

--- backend/challenge/urls.py
+++ backend/challenge/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from challenge.views import ChallengeListView, ChallengeRetrieveView
+from challenge.views import ChallengeListView, ChallengeRetrieveView, ChallengeFileDownloadViewSet
+
+router = routers.DefaultRouter()
+
+router.register("challenge-file-download", ChallengeFileDownloadViewSet)
 
 urlpatterns = [
     path(r'challenges/', ChallengeListView.as_view()),
     path(r'challenges/<int:pk>/', ChallengeRetrieveView.as_view()),
+    # path(r'', include(router.urls)),
+    path(r'challenge-file-download/<int:pk>/', ChallengeFileDownloadViewSet.as_view(
+        {
+            'get': 'download'
+        }
+    ))
 ]
--- backend/challenge/views.py
+++ backend/challenge/views.py
@@ -1,7 +1,11 @@
+from django.http import FileResponse
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets, renderers, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
+from account.models import UserChallengeSession
 from challenge.models import Challenge
 from challenge.serializer import ChallengeSerializer
 from utils.custom_permissions import IsActivatedUser
@@ -17,3 +21,47 @@ class ChallengeRetrieveView(generics.RetrieveAPIView):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
     permission_classes = [IsActivatedUser, IsAuthenticated]
+
+
+class PassThroughRenderer(renderers.BaseRenderer):
+    media_type = ""
+    format = ""
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        return data
+
+
+class ChallengeFileDownloadViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Challenge.objects.all()
+    permission_classes = [IsAuthenticated, IsActivatedUser]
+
+    @action(methods=['get'], detail=True, renderer_classes=(PassThroughRenderer,))
+    def download(self, request, *args, **kwargs):
+        instance = self.get_object()
+        session = UserChallengeSession.objects.filter(user=request.user, challenge=instance).last()
+        if not session:
+            return Response(
+                data={"detail": "You haven't opened a challenge session yet."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        print(session)
+
+        if session.get_current_state():
+            return Response(
+                data={"detail": "Your session has been closed."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not instance.file:
+            return Response(
+                data={"detail": "This Challenge has no file to download."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        file_handle = instance.file.open()
+
+        response = FileResponse(file_handle, content_type="application/x-rar-compressed")
+        response['Content-Length'] = instance.file.size
+        response['Content-Disposition'] = 'attachment; filename="%s"' % instance.file.name
+
+        return response
