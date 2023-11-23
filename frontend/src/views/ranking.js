a799eb367efe377bd1ebc6577e24a799d9c2326c
DopamineNone <123919976+DopamineNone@users.noreply.github.com>
2023-11-23 09:26:04 +0800

    fix canvas malposition error

--- frontend/src/views/ranking.js
+++ frontend/src/views/ranking.js
@@ -10,9 +10,12 @@ export default {
   target: 'main',
   methods: {
     // 获取用户
-    showLineChart: async (page) => {
+    showUserLineChart: async function(page) {
       // 获取信息
-      const results = await getUserRanking(page)
+      const response = await getUserRanking(page)
+      const results = response.results
+      // 记录用户数
+      document.getElementById('rank').setAttribute('data-usernum', response.count)
       // 获取失败
       if (results === null) {
         return
@@ -103,44 +106,137 @@ export default {
       chart.setOption(option)
       // 绑定resize事件，实现自适应
       window.addEventListener('resize', chart.resize)
+      this.userChart = chart
     },
-    getTeamGrade: async (page) => { 
+    showTeamLineChart: async function(page) { 
       // 获取信息
-      const results = await getTeamRanking(page)
+      const response = await getTeamRanking(page)
+      const results = response.results
+      // 记录战队数
+      document.getElementById('rank').setAttribute('data-teamnum', response.count)
       // 获取失败
       if (results === null) {
         return
       }
-      // 获取用户名列表、积分数据
-      const userList = []
+      // 折线图
+      // 获取战队名列表、积分数据
+      const teamList = []
       const series = []
       results.forEach(item => {
         // 用户名
-        userList.push(item.username)
+        teamList.push(item.name)
         // series 用于构成坐标点数据
         const data = []
-        for (let obj of item.score_set){
+        for (let obj of item.teamscore_set){
           data.push([
             obj.solved_at,
             obj.current_points
           ])
         }
         series.push({
-          name: item.username,
+          name: item.name,
           type: 'line',
           data
         })
       })
+      // 折线图配置信息
+      const option = {
+        // 标题
+        title: {
+          text: '战队积分折线图',
+          x: 'center',
+          y: 'top',
+          textAlign: 'left',
+          textStyle: {
+            color: '#818CF8'
+          },
+        },
+        // 图例组件
+        legend: {
+          show: true,
+          top: '10%',
+          data: teamList,
+          textStyle: {
+            color: '#818CF8',
+          },
+        },
+        grid: {
+          x: '7%',
+          y: '20%',
+          x2: '5%',
+          y2: '10%',
+        },
+        // x轴 时间
+        xAxis: {
+          type: 'time',
+          axisLabel: {
+            // 展示格式
+            formatter: function (dateString){
+              const t_date = new Date(dateString);
+              return (t_date.getMonth() + 1) + '-' + t_date.getDate()
+            },
+            textStyle: {
+              color: '#818CF8',
+            },
+          },
+        },
+        // y轴 积分值
+        yAxis: {
+          type: 'value',
+          axisLabel: {
+            textStyle: {
+              color: '#818CF8',
+            },
+          },
+        },
+        // series 数据（坐标）
+        series,
+        // tooltip 提示框
+        tooltip: {
+          trigger: 'item',
+          formatter: function(params){
+            return params.marker + params.seriesName + '<br>' + '积分：' + params.data[1] + '<br />时间：' + params.data[0]
+          }
+        }
+      }
+      // 渲染折线图
+      const chart = echarts.init(document.querySelector('#team-rank>.chart'))
+      chart.setOption(option)
+      // 绑定resize事件，实现自适应
+      window.addEventListener('resize', chart.resize)
+      this.teamChart = chart
     },
+    destroyLineChart: function() {
+      this.userChart.dispose()
+      this.teamChart.dispose()
+    },
+    switchView: function(e) {
+      // 过滤被点击的input
+      if (e.target.tagName === 'LABEL'){
+        if (e.target.id === 'user-view'){
+          //隐藏team-rank
+          document.getElementById('team-rank').classList.add('hide')
+          // 显示user-rank
+          document.getElementById('user-rank').classList.remove('hide')
+        }
+        else {
+          //隐藏user-rank
+          document.getElementById('user-rank').classList.add('hide')
+          // 显示team-rank
+          document.getElementById('team-rank').classList.remove('hide')
+          
+        }
+      }
+    }
   },
   template: `
     <div id="rank">
-      <div class="radio-inputs">
-        <label class="radio">
+      <div id="radio-inputs">
+        <label class="radio" id="user-view">
           <input type="radio" name="radio" checked>
           <span class="name">个人排名</span>
         </label>
-        <label class="radio">
+        <label class="radio" id="team-view">
           <input type="radio" name="radio">
           <span class="name">战队排名</span>
         </label>
@@ -159,7 +255,7 @@ export default {
             <button>确认</button>
         </form>
       </div>
-      <div class="card rank" id="team-rank" style="display: none">
+      <div class="card rank hide" id="team-rank">
         <div class="chart"></div>
         <div class="table"></div>
         <p class="pagenum"></p>
@@ -180,9 +276,13 @@ export default {
     return this.template
   },
   afterMount: function() {
-    // 获取前十用户排名折线图
-    this.methods.showLineChart(1)
+    // 获取前十用户&战队排名折线图
+    this.methods.showUserLineChart(1)
+    this.methods.showTeamLineChart(1)
+    document.getElementById('radio-inputs').addEventListener('click', this.methods.switchView)
   },
   destroyed: function() {
+    document.getElementById('radio-inputs').removeEventListener('click', this.methods.switchView)
+    this.methods.destroyLineChart()
   }
 }
