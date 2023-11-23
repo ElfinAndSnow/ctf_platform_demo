169ef68e82bf9c5524d3205c3aba253286f22619
DopamineNone <123919976+DopamineNone@users.noreply.github.com>
2023-11-23 18:46:53 +0800

    fix login view error

--- frontend/src/utils/preload.js
+++ frontend/src/utils/preload.js
@@ -2,9 +2,15 @@ import '../assets/css/loader.css'
 import { verify, getUserInfo } from '../api/api.js'
 // 预加载器
 export async function preload(){
+    // 验证是否登录
+    const res = await verify()
+    if (res){
+        document.body.classList.add('logined')
+    }
     // 载入加载动画
     document.onreadystatechange = () => {
         const loader = document.querySelector(".box")
+
         switch (document.readyState) {
             case 'loading': 
                 loader.style.display = 'block'
