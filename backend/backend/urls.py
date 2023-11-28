91269d23e2b60a3d594697a1b5248d546d37c4d1
ElfinAndSnow <andyfucookies@gmail.com>
2023-11-28 15:23:29 +0800

    fix main urlconf

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,13 +18,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.documentation import include_docs_urls
-
-from library.views import BookViewSet
-from rest_framework import routers
-# import jwtauth.urls as jwturls
-from django.urls import path
-from account import models
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -46,26 +39,13 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # 接口文档，开放访问权限用于前端开发
-    # 以后使用swagger
-    # path(r'docs/', include_docs_urls(title="接口文档", authentication_classes=[], permission_classes=[])),
+
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path(r'api/', include('library.urls')),
-    path(r'api/', include('article.urls')),
     path(r'api/', include('account.urls')),
     path(r'api/', include('leaderboard.urls')),
-
-    # path(r'api/', include('puzzles.urls')),
     path(r'api/', include('challenge.urls')),
-
-    # login, register, token
     path(r'auth/', include('jwtauth.urls')),
-
-    # team
-    path(r'api/', include('team.urls'))
+    path(r'api/', include('team.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# urlpatterns += router.urls
